f236674609de95a570b6b9f27e9b582dafcab8f6
Nisith <nisith.bpt@gmail.com>
2025-06-06 10:18:43 +0200

    first commit

---
+++ src/main/java/com/vaccination/services/api/gateway/CitizenVaccineApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.vaccination.services.api.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitizenVaccineApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitizenVaccineApiGatewayApplication.class, args);
+	}
+
+}
